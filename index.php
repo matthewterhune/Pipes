dd42f858bf92d117c2c8872bd3330a24e1606a0c
CTS-Network Engineer 4 (Matt Terhune) <cts-neteng4@sil.org.pg>
2019-09-20 14:28:58 +1000

    First commit

---
+++ index.php
@@ -0,0 +1,350 @@
+<!DOCTYPE html>
+<html lang="en-US">
+<head>
+<title>Tile Twist</title>
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<meta name="Keywords" content="game,tree,trees,maze,fun,puzzle,test,bored">
+<meta name="Description" content="Unscramble tiles to complete the puzzle!">
+<link rel="stylesheet" type="text/css" href="./tree.css">
+<link href="https://fonts.googleapis.com/css?family=Audiowide" rel="stylesheet">
+
+<script type="text/javascript">
+	
+	function checkTree(x, y, max=(x * 2)) {
+
+		var count = 1;
+
+
+		var myobject = document.getElementById(String(x) + "x" + String(y));
+		var myimages = myobject.getElementsByTagName('img');
+
+		if (y > 0) {
+			var topobject = document.getElementById(String(x) + "x" + String(y - 1));
+			var topimages = topobject.getElementsByTagName('img');
+		}
+		if (x < max) {
+			var rightobject = document.getElementById(String(x + 1) + "x" + String(y));
+			var rightimages = rightobject.getElementsByTagName('img');
+		}
+		if (y < max) {
+			var bottomobject = document.getElementById(String(x) + "x" + String(y + 1));
+			var bottomimages = bottomobject.getElementsByTagName('img');
+		}
+		if (x > 0) {
+			var leftobject = document.getElementById(String(x - 1) + "x" + String(y));
+			var leftimages = leftobject.getElementsByTagName('img');
+		}
+		if (y > 0) {
+			if (!(myimages[0].classList.contains('hideme'))) {
+				if (!(topimages[2].classList.contains('hideme'))) {
+					if (!(topobject.classList.contains('filled'))) {
+						topobject.classList.add('filled');
+						count += checkTree(x, y - 1, max);
+					}
+				}
+			}
+		}
+		if (x < max) {
+			if (!(myimages[1].classList.contains('hideme'))) {
+				if (!(rightimages[3].classList.contains('hideme'))) {
+					if (!(rightobject.classList.contains('filled'))) {
+						rightobject.classList.add('filled');
+						count += checkTree(x + 1, y, max);
+					}
+				}
+			}
+		}
+		if (y < max) {
+			if (!(myimages[2].classList.contains('hideme'))) {
+				if (!(bottomimages[0].classList.contains('hideme'))) {
+					if (!(bottomobject.classList.contains('filled'))) {
+						bottomobject.classList.add('filled');
+						count += checkTree(x, y + 1, max);
+					}
+				}
+			}
+		}
+		if (x > 0) {
+			if (!(myimages[3].classList.contains('hideme'))) {
+				if (!(leftimages[1].classList.contains('hideme'))) {
+					if (!(leftobject.classList.contains('filled'))) {
+						leftobject.classList.add('filled');
+						count += checkTree(x - 1, y, max);
+					}
+				}
+			}
+		}
+		return count;
+	}
+
+	function savescores(scores) {
+		var urlstring = "scores=" + scores;
+		var xhttp = new XMLHttpRequest();
+		xhttp.onreadystatechange = function() {
+			if (this.readyState == 4 && this.status == 200) {
+				document.getElementById('newboard').click();
+			}
+		};
+		xhttp.open("POST", "writescores.php", true);
+		xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+		xhttp.send(urlstring);
+	}
+
+	function submitscore() {
+		var myscores = document.getElementById('scorecontainer').getElementsByTagName('tr');
+		var submitname = JSON.stringify(document.getElementById('submitname').value).replace(/(\r\n|\n|\r|,|")/gm, ' ');
+		var newscore = [submitname, Number(document.getElementById('bestscore').innerHTML)];
+		var scorearray = [];
+		
+		for (i=0; i<10; i++) {
+			scorearray.push([myscores[i].getElementsByClassName("scorename")[0].innerHTML, Number(myscores[i].getElementsByClassName("score")[0].innerHTML)]);
+		}
+		scorearray.push(newscore);
+		scorearray.sort(function(a,b){return b[1] - a[1];});
+		var printstring = "";
+		for (i=0; i<10; i++) {
+			printstring += scorearray[i][0] + "," + String(scorearray[i][1]) + "%0A";
+		}
+		savescores(printstring);
+		// document.getElementById('newboard').click();
+	}
+
+	function winner() {
+		clearInterval(timerID);
+		document.getElementById('victory').style.display = 'block';
+		document.getElementById('gameboard').classList.add('winner');
+		var scores = document.getElementsByClassName('score');
+		var lowscore = Number(scores[scores.length - 1].innerHTML);
+		var bestscore = Number(document.getElementById('bestscore').innerHTML)
+		var movecount = Number(document.getElementById('movecount').innerHTML)
+		if (movecount < bestscore) {
+			if (bestscore > lowscore) {
+				document.getElementById('submitscorecontainer').classList.add('show');
+			}
+		}
+	}
+
+	function updateTree(center) {
+		max = center + center;
+		for (j=0; j <= max; j++) {
+			for (i=0; i <= max; i++) {
+				document.getElementById(String(i) + "x" + String(j)).classList.remove("filled");
+			}
+		}
+		var finalcount = checkTree(center, center, max);
+		var piececount = document.getElementById('usedpieces')
+		if (piececount.innerHTML == "") {
+			piececount.innerHTML = String(finalcount);
+		} else if (piececount.innerHTML == String(finalcount)) {
+			winner();
+		}
+	}
+
+	function rotatePiece(myobject) {
+		var myimages = myobject.getElementsByTagName('img');
+		var newleft, newtop, newbottom, newright = false;
+		
+		if (myimages[0].classList.contains('hideme')) {
+			newright = true;
+		}
+		if (myimages[1].classList.contains('hideme')) {
+			newbottom = true;
+		}
+		if (myimages[2].classList.contains('hideme')) {
+			newleft = true;
+		}
+		if (myimages[3].classList.contains('hideme')) {
+			newtop = true;
+		}
+
+		if (newtop) {
+			myimages[0].classList.add("hideme");
+		} else {
+			myimages[0].classList.remove("hideme");
+		}
+		if (newright) {
+			myimages[1].classList.add("hideme");
+		} else {
+			myimages[1].classList.remove("hideme");
+		}
+		if (newbottom) {
+			myimages[2].classList.add("hideme");
+		} else {
+			myimages[2].classList.remove("hideme");
+		}
+		if (newleft) {
+			myimages[3].classList.add("hideme");
+		} else {
+			myimages[3].classList.remove("hideme");
+		}
+	}
+
+	function getRandomInt(min, max) {
+		min = Math.ceil(min);
+		max = Math.floor(max);
+		return Math.floor(Math.random() * (max - min)) + min;
+	}
+
+	function incrementMinutes() {
+		var mytime = document.getElementById('minutes');
+		var timenumber = (Number(mytime.innerHTML) + 1);
+		if (timenumber < 10) {
+			mytime.innerHTML = "0" + String(timenumber);
+		} else {
+			mytime.innerHTML = String(timenumber);
+		}
+	}
+
+	function incrementClock() {
+		var mytime = document.getElementById('time');
+		var timenumber = (Number(mytime.innerHTML) + 1);
+		if (timenumber < 10) {
+			mytime.innerHTML = "0" + String(timenumber);
+		} else if (timenumber == 60) {
+			mytime.innerHTML = "00";
+			incrementMinutes();
+		} else {
+			mytime.innerHTML = String(timenumber);
+		}
+	}
+
+	function scrambleBoard(myobject, mycenter) {
+		var mytds = myobject.getElementsByTagName('td');
+		var blanktds = myobject.getElementsByClassName('blank');
+		var bestscore = 0;
+
+		for (i=0; i<mytds.length; i++) {
+			if (!(mytds[i].classList.contains('blank'))) {
+				if (mytds[i].classList.contains('line')) {
+					var times = getRandomInt(1, 3);
+					while (times > 0) {
+						rotatePiece(mytds[i]);
+						times -= 1;
+						bestscore += 1;
+					}
+					bestscore = bestscore + 2;
+				} else {
+					var times = getRandomInt(1, 5);
+					while (times > 0) {
+						rotatePiece(mytds[i]);
+						times -= 1;
+						bestscore += 1;
+					}
+				}
+			}
+		}
+		document.getElementById("bestscore").innerHTML = String(((mytds.length - blanktds.length)*4)-bestscore);
+		return setInterval("incrementClock()", 1000);
+	}
+
+	function hideme() {
+		document.getElementById('scrambleoverlay').style.display = 'none';
+	}
+
+	function increment() {
+		myobject = document.getElementById('movecount');
+		myobject.innerHTML = String(Number(myobject.innerHTML) + 1);
+	}
+
+	function incrementsize(amount) {
+		var myobject = document.getElementById('size');
+		var currentsize = Number(myobject.value);
+		if (Number.isNaN(currentsize)) {
+			currentsize = 11;
+		}
+		currentsize = currentsize + amount;
+		if (currentsize < 3) {
+			currentsize = 3;
+		}
+		if (currentsize > 31) {
+			currentsize = 31;
+		}
+		myobject.value = String(currentsize);
+	}
+
+</script>
+
+</head>
+<body>
+
+<div id="main">
+<div id="boardcontainer">
+<div id="scrambleoverlay" onclick="timerID = scrambleBoard(document.getElementById('gameboard'), centerid); updateTree(centernum); hideme();"></div>
+<div id="victory">
+	<div id="submitscorecontainer">
+	<div>
+	<p>Perfect score! Add your name to the high scores list:</p>
+	<input id="submitname" type="text" maxlength="16" placeholder="Enter Your Name">
+	<button class="controlbutton" type="button" onclick="submitscore();">Submit</button>
+	</div>
+	</div>
+</div>
+<?php
+
+if( isset($_POST['size']) )
+{
+	$command = escapeshellcmd('python tree.py ' . $_POST['size']);
+	$mysize = $_POST['size'];
+} else {
+	$command = escapeshellcmd('python tree.py 9');
+	$mysize = "9";
+}
+$output = shell_exec($command);
+echo $output;
+
+?>
+</div>
+
+<div id="controls">
+<h1 id="title">TILET<span id="twist">w</span>IST</h1>
+	<form class="countcontainer" method="post" action="index.php" onsubmit="incrementsize(0);">
+		<div id="sizediv">
+			<span id="sizelabel">Size:</span>
+			<input id="size" type="text" name="size" value="<?php echo $mysize; ?>">
+			<img class="arrows" src="./arrow-up.png" onclick="incrementsize(2);"><img class="arrows" src="./arrow-down.png" onclick="incrementsize(-2);">
+		</div>
+		<input class="controlbutton" type="submit" id="newboard" value="New">
+		<button class="controlbutton" type="button" id="scramble" onclick="timerID = scrambleBoard(document.getElementById('gameboard'), centerid); updateTree(centernum); hideme();">Scramble</button>
+	</form>
+
+	<table class="countcontainer">
+		<tr><td>Time:</td><td class="align-right"><span id="minutes">00</span>:<span id="time">00</td></tr>
+		<tr><td>Moves:</td><td class="align-right"><span id="movecount">0</span>/<span id="bestscore">0</span></td></tr>
+	</table>
+
+	<!--<div id="description" class="countcontainer">
+		Click "New" to create a new board, then "Scramble" to mix up the tiles. Click the tiles to rotate them and connect a path from the center tile to the rest of the board.
+	</div>-->
+
+	<table id="scorecontainer" class="scorecontainer">
+		<caption>Perfect Scores</caption>
+		<?php
+			$count = 1;
+			$myfile = file("./scores/perfect", FILE_IGNORE_NEW_LINES);
+			foreach ($myfile as $myline) {
+				$myvalues = explode(",", $myline);
+				echo "<tr id='score" . $count . "'><td class='scorename'>" . $myvalues[0] . "</td><td class='align-right'><span class='score'>" . $myvalues[1] . "</span></td></tr>";
+				$count++;
+			}
+		?> 
+		</table>
+</div>
+
+</div>
+
+<script type="text/javascript">
+	var boardcenter = document.getElementById('boardcenter').innerHTML;
+	var centerid = boardcenter + "x" + boardcenter;
+	var centernum = Number(boardcenter)
+	timerID = 0;
+	document.getElementById(centerid).classList.add('centerpiece');
+	document.getElementById(centerid).classList.add('filled');
+	document.getElementById(centerid).classList.add('blank');
+	document.getElementById(centerid).getElementsByClassName('overlay')[0].onclick = "";
+	updateTree(centernum);
+</script>
+
+</body>
+</html>
+
---
+++ tree.py
@@ -0,0 +1,257 @@
+# encoding: utf-8
+
+import random
+import sys
+
+
+class Node:
+
+	def __init__(self, xpos=0, ypos=0, top=False, left=False, bottom=False, right=False):
+		self.top = top
+		self.bottom = bottom
+		self.left = left
+		self.right = right
+		self.xpos = xpos
+		self.ypos = ypos
+
+	def count(self):
+		sides = 0
+		if self.top:
+			sides += 1
+		if self.bottom:
+			sides += 1
+		if self.left:
+			sides += 1
+		if self.right:
+			sides += 1
+		return sides
+
+
+class Tree:
+
+	branchpercent = 30
+	turnpercent = 40
+
+	def __init__(self, size=7, startx=4, starty=1):
+		self.map = [[Node(xpos=j, ypos=i) for i in range(size)] for j in range(size)]
+		self.endpoints = [{"x": (size/2), "y": (size/2), "dead": False}]
+		self.map[(size/2)][(size/2)].top = True
+		self.size = size
+
+	def movepoint(self, point, direction):
+		if (direction == "down"):
+			if (point["y"] < (self.size - 1)):
+				if (self.map[point["x"]][point["y"]+1].count() == 0):
+					self.map[point["x"]][point["y"]].bottom = True
+					self.map[point["x"]][point["y"]+1].top = True
+					point["y"] += 1
+					return True
+		if (direction == "up"):
+			if (point["y"] > 0):
+				if (self.map[point["x"]][point["y"]-1].count() == 0):
+					self.map[point["x"]][point["y"]].top = True
+					self.map[point["x"]][point["y"]-1].bottom = True
+					point["y"] -= 1
+					return True
+		if (direction == "right"):
+			if (point["x"] < (self.size - 1)):
+				if (self.map[point["x"]+1][point["y"]].count() == 0):
+					self.map[point["x"]][point["y"]].right = True
+					self.map[point["x"]+1][point["y"]].left = True
+					point["x"] += 1
+					return True
+		if (direction == "left"):
+			if (point["x"] > 0):
+				if (self.map[point["x"]-1][point["y"]].count() == 0):
+					self.map[point["x"]][point["y"]].left = True
+					self.map[point["x"]-1][point["y"]].right = True
+					point["x"] -= 1
+					return True
+		return False
+
+	def randomside(self, point):
+		ways = []
+		if (point["y"] < (self.size - 1)):
+			if (self.map[point["x"]][point["y"]+1].count() == 0):
+				ways.append("down")
+		if (point["y"] > 0):
+			if (self.map[point["x"]][point["y"]-1].count() == 0):
+				ways.append("up")
+		if (point["x"] < (self.size - 1)):
+			if (self.map[point["x"]+1][point["y"]].count() == 0):
+				ways.append("right")
+		if (point["x"] > 0):
+			if (self.map[point["x"]-1][point["y"]].count() == 0):
+				ways.append("left")
+		if (len(ways) == 0):
+			ways.append("none")
+		myresult = random.choice(ways)
+		return myresult
+
+	def randompoint(self, point):
+		self.movepoint(point, self.randomside(point))
+
+	def cycle(self):
+		for endpoint in self.endpoints[:]:
+			#print endpoint
+			x = endpoint["x"]
+			y = endpoint["y"]
+			if (endpoint["dead"]):
+				continue
+			if (self.randomside(endpoint) == "none"):
+				endpoint["dead"] = True
+				continue
+			if (random.randint(0,100) < self.branchpercent):
+				self.endpoints.append({"x": x, "y": y, "dead": False})
+				continue
+			if (random.randint(0,100) < self.turnpercent):
+				self.randompoint(endpoint)
+				continue
+			if self.map[x][y].top:
+				self.movepoint(endpoint, "down")
+			if self.map[x][y].bottom:
+				self.movepoint(endpoint, "up")
+			if self.map[x][y].left:
+				self.movepoint(endpoint, "right")
+			if self.map[x][y].right:
+				self.movepoint(endpoint, "left")
+
+	def fixCenter(self):
+		if (self.map[(self.size/2)][(self.size/2) - 1].bottom == True):
+			self.map[self.size/2][self.size/2].top = True
+		else:
+			self.map[self.size/2][self.size/2].top = False
+		if (self.map[(self.size/2) + 1][(self.size/2)].left == True):
+			self.map[self.size/2][self.size/2].right = True
+		else:
+			self.map[self.size/2][self.size/2].right = False
+		if (self.map[(self.size/2)][(self.size/2) + 1].top == True):
+			self.map[self.size/2][self.size/2].bottom = True
+		else:
+			self.map[self.size/2][self.size/2].bottom = False
+		if (self.map[(self.size/2) - 1][(self.size/2)].right == True):
+			self.map[self.size/2][self.size/2].left = True
+		else:
+			self.map[self.size/2][self.size/2].left = False
+
+	def printNode(self, xvalue=0, yvalue=0):
+		return self.map[xvalue][yvalue].left
+
+	def uniBox(self, top=False, right=False, bottom=False, left=False):
+		charset = {"top": u"┃",
+				   "topright": u"┗",
+				   "toprightbottom": u"┣",
+				   "toprightbottomleft": u"╋",
+				   "toprightleft": u"┻",
+				   "topbottomleft": u"┫",
+				   "topleft": u"┛",
+				   "topbottom": u"┃",
+				   "right": u"━",
+				   "rightbottom": u"┏",
+				   "rightbottomleft": u"┳",
+				   "rightleft": u"━",
+				   "bottom": u"┃",
+				   "bottomleft": u"┓",
+				   "left": u"━"}
+		indexstring = ""
+		if top:
+			indexstring = "top"
+		if right:
+			indexstring = indexstring + "right"
+		if bottom:
+			indexstring = indexstring + "bottom"
+		if left:
+			indexstring = indexstring + "left"
+		if (indexstring == ""):
+			return u"•"
+		else:
+			return charset[indexstring]
+
+
+	def printBoard(self):
+		print ""
+		for j in range(0, self.size):
+			printstring = u""
+			for i in range(0, self.size):
+				printstring = printstring + self.uniBox(self.map[i][j].top, self.map[i][j].right, self.map[i][j].bottom, self.map[i][j].left)
+				#printstring += str(self.map[i][j].count())
+				#printstring += str(self.map[i][j].ypos)
+			print printstring
+		#print sorted(self.endpoints, key=itemgetter('y'))
+
+	def printHTML(self):
+		self.fixCenter()
+		printstring = "<table id='gameboard' class='piece'>\n"
+		squaresize = "40"
+		for j in range(0, self.size):
+			printstring += "	<tr>\n"
+			for i in range(0, self.size):
+				printstring += "		<td id='" + str(i) + "x" + str(j) + "'"
+				if (self.map[i][j].top == False):
+					if (self.map[i][j].bottom == False):
+						if (self.map[i][j].left == False):
+							if (self.map[i][j].right == False):
+								printstring += " class='blank'"
+						if (self.map[i][j].left == True):
+							if (self.map[i][j].right == True):
+								printstring += " class='line'"
+				if (self.map[i][j].top == True):
+					if (self.map[i][j].bottom == True):
+						if (self.map[i][j].left == False):
+							if (self.map[i][j].right == False):
+								printstring += " class='line'"
+						if (self.map[i][j].left == True):
+							if (self.map[i][j].right == True):
+								printstring += " class='blank'"
+				printstring += ">\n"
+
+				printstring += "		<div class='underlay'></div>\n"
+
+				printstring += "		<img class='TL"
+				if (self.map[i][j].top == False):
+					printstring += " hideme"
+				printstring += "' src='T" + squaresize + ".png'>\n"
+
+				printstring += "		<img class='RL"
+				if (self.map[i][j].right == False):
+					printstring += " hideme"
+				printstring += "' src='R" + squaresize + ".png'>\n"
+
+				printstring += "		<img class='BL"
+				if (self.map[i][j].bottom == False):
+					printstring += " hideme"
+				printstring += "' src='B" + squaresize + ".png'>\n"
+
+				printstring += "		<img class='LL"
+				if (self.map[i][j].left == False):
+					printstring += " hideme"
+				printstring += "' src='L" + squaresize + ".png'>\n"
+				printstring += "<div onclick='rotatePiece(this.parentNode); updateTree(centernum); increment();' class='overlay'>.</div>\n"
+
+				printstring += "		</td>\n"
+
+			printstring += "	</tr>\n"
+		printstring += "</table>\n\n"
+		printstring += "<div id='boardcenter' class='boardinfo'>" + str(self.size/2) + "</div>"
+		printstring += "<div id='usedpieces' class='boardinfo'></div>"
+
+		print printstring
+		#print sorted(self.endpoints, key=itemgetter('y'))
+
+
+
+
+mytree = Tree(size=int(sys.argv[1]))
+
+for i in range(100):
+	mytree.cycle()
+
+mytree.printHTML()
+
+
+
+
+
+
+
+
